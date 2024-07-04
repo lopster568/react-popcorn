42dcdd1f2fdcb3b0bab96010e90f9f5e16b1089b
Roshan Singh <roshan.s@wijungle.com>
2024-07-04 13:25:55 +0530

    Deploy Webapp to Github Pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import MoviePage from './pages/movie/movie';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
